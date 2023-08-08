9c047aacb5403877f20a51d13cd6b0f0af64f4a9
danieladeeri <danieladeeri@gmail.com>
2023-08-08 10:14:58 +0100

    Created new welcome page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'welcome',
+    loadChildren: () => import('./welcome/welcome.module').then( m => m.WelcomePageModule)
+  },
 ];
 
 @NgModule({
